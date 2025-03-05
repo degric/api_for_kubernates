6b130180532b272415df398c1f528d71f0972b4b
degric <ricardodegante48@gmail.com>
2025-03-05 12:15:42 -0600

    change if __name

--- src/app.py
+++ src/app.py
@@ -41,6 +41,4 @@ def test():
     data = getData('fiction')
     return data
 
-
-if __name__ == "__main__":
-    app.run(port=8888, debug=True, host="0.0.0.0")
+app.run(port=8888, debug=True, host="0.0.0.0")
